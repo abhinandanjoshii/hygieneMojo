8b7e08fff15f1d82e19b09c3d16e77b797b8669f
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2026-06-04 03:41:27 +0530

    Create initial HygieneMojo goal

--- src/main/java/io/github/abhinandanjoshii/hygiene/HygieneMojo.java
+++ src/main/java/io/github/abhinandanjoshii/hygiene/HygieneMojo.java
@@ -1,5 +1,14 @@
 package io.github.abhinandanjoshii.hygiene;
 
-public class HygieneMojo {
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name="check")
+public class HygieneMojo extends AbstractMojo{
+
+    @Override
+    public void execute(){
+        getLog().info("HygieneMojo running");
+    }
 
 }
