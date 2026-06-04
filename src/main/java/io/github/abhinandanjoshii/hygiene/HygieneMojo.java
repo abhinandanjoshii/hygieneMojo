f7c91fa8f7d2faa85a677030b9788a42f5b461c7
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2026-06-04 16:59:25 +0530

    Add README existence validation - v0

--- src/main/java/io/github/abhinandanjoshii/hygiene/HygieneMojo.java
+++ src/main/java/io/github/abhinandanjoshii/hygiene/HygieneMojo.java
@@ -1,14 +1,33 @@
 package io.github.abhinandanjoshii.hygiene;
 
 import org.apache.maven.plugin.AbstractMojo;
+import java.io.File;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name="check")
 public class HygieneMojo extends AbstractMojo{
 
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
     @Override
     public void execute(){
         getLog().info("HygieneMojo running");
+
+        File readme = new File(
+                project.getBasedir(),
+                "README.md"
+        );
+
+        if(readme.exists()){
+            getLog().info("README.md found.");
+        }
+        else {
+            getLog().warn("README.md missing.");
+        }
+        getLog().info("project root : "+ project.getBasedir().getAbsolutePath());
     }
 
 }
