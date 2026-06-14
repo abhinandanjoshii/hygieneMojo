36d5d65ce5e4c15c20c569342ac6a7ff9149df9e
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2026-06-15 00:21:09 +0530

    adding report generation architecture

---
+++ src/main/java/io/github/abhinandanjoshii/hygiene/report/HygieneReport.java
@@ -0,0 +1,6 @@
+package io.github.abhinandanjoshii.hygiene.report;
+
+public class HygieneReport {
+
+    "Just a class -> final use case would be maybe for instanciation ig :)"
+}
---
+++ src/main/java/io/github/abhinandanjoshii/hygiene/report/HygieneReportWriter.java
@@ -0,0 +1,9 @@
+package io.github.abhinandanjoshii.hygiene.report;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+public class HygieneReportWriter {
+    public static NotImplementedException write(){
+        return new NotImplementedException("SOOOOON!");
+    }
+}
